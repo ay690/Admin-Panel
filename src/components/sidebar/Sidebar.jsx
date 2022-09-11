3f23e9d08c1cc3d8f0db5c1b2dc5e276658e49b8
Aniket Yadav <aniketyadav690@gmail.com>
2022-09-11 18:29:29 +0530

    React dark Mode Done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import "./sidebar.scss";
-import DashboardIcon from '@mui/icons-material/Dashboard';
+import DashboardIcon from "@mui/icons-material/Dashboard";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import CreditCardIcon from "@mui/icons-material/CreditCard";
@@ -12,38 +11,35 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import {Link} from 'react-router-dom'
-import { useContext } from 'react';
-import { DarkModeContext } from '../../context/darkModeContext';
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
 
 const Sidebar = () => {
-
-  const {dispatch} = useContext(DarkModeContext);
-
+  const { dispatch } = useContext(DarkModeContext);
   return (
-    <div className = 'sidebar'>
-        <div className="top">
-          <Link to = "/" style={{textDecoration:"none"}}>
-          <span className="logo">Admin</span>
-          </Link>  
-        </div>
-        <hr/>
-        <div className="center">
-            <ul>
-            <p className="title">MAIN</p>
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">lamadmin</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
           <li>
             <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
           <p className="title">LISTS</p>
-          <Link to = "/users" style = {{textDecoration:"none"}}>
+          <Link to="/users" style={{ textDecoration: "none" }}>
             <li>
               <PersonOutlineIcon className="icon" />
               <span>Users</span>
             </li>
           </Link>
-
-          <Link to = '/products' style = {{textDecoration:"none"}}>
+          <Link to="/products" style={{ textDecoration: "none" }}>
             <li>
               <StoreIcon className="icon" />
               <span>Products</span>
@@ -88,14 +84,20 @@ const Sidebar = () => {
             <ExitToAppIcon className="icon" />
             <span>Logout</span>
           </li>
-            </ul>
-        </div>
-        <div className="bottom">
-          <div className="colorOption" onClick={() => dispatch({type:"LIGHT"})} ></div>
-          <div className="colorOption" onClick={() => dispatch({type:"DARK"})} ></div>
-        </div>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
