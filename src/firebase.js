13e7c56cfb20859c9c8eb0334b8d2d3e41b8b134
Aniket Yadav <aniketyadav690@gmail.com>
2022-09-21 18:04:18 +0530

    firebase setup done

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "admin-project-60d0e.firebaseapp.com",
+  projectId: "admin-project-60d0e",
+  storageBucket: "admin-project-60d0e.appspot.com",
+  messagingSenderId: "223721413846",
+  appId: "1:223721413846:web:41a4e6ecc82cffa1a93d77"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
